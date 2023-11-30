20342b9d8278fb55d967ca6ebfd72676670aa4c3
Ales Dumikau <ales.dumikau@altoros.com>
2023-11-30 15:42:07 +0300

    Remove dependencies from installation script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-
+#python3 -m venv venv
+#source venv/bin/activate
+#pip install -r requirements.txt
+#
 cp ./mycelium-healthcheck.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable mycelium-healthcheck
