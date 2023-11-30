0e05e9a87644650b52b345d266429922f9c753a8
Ales Dumikau <ales.dumikau@altoros.com>
2023-11-30 16:52:29 +0300

    Add endpoint for Mycelium monitoring

--- main.py
+++ main.py
@@ -11,10 +11,7 @@ app.logger.setLevel(logging.INFO)
 handler = logging.FileHandler('app.log')
 app.logger.addHandler(handler)
 
-@app.route('/health')
-def main():
-    url = "http://localhost:8545/"
-
+def is_eth_endpoint_healthy(url):
     payload = {
         "jsonrpc": "2.0",
         "method": "eth_blockNumber",
@@ -36,4 +33,14 @@ def main():
     if "error" in intermediate_response_json:
         return {"is_up": False, "reason": intermediate_response.content}, 500
     
-    return {"is_up": True}, 200 
+    return {"is_up": True}, 200
+
+@app.route("/up")
+def is_fleet_healthy():
+    url = "https://api.mycelium.calibration.node.glif.io/"
+    return is_eth_endpoint_healthy(url)
+
+@app.route('/health')
+def is_local_edpoint_healthy():
+    url = "http://localhost:8545/"
+    return is_eth_endpoint_healthy(url)
